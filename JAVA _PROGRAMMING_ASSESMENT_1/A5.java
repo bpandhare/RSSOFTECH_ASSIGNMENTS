e58582e02ed4bd6b25247ba8432a97957211791d
Bharati2601 <bharatipandhare2601@gmail.com>
2026-04-13 23:13:32 +0530

    Add JAVA_PROGRAMMING_ASSESMENT_1 folder

---
+++ JAVA _PROGRAMMING_ASSESMENT_1/A5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class A5{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int a= sc.nextInt();
+ 	
+	if(a>0){
+        	System.out.println("Given number is positive");
+
+	     }
+	else if(a<0){
+        	System.out.println("Given number is negative");
+
+	     }
+
+         else {
+        	System.out.println("Given number is zero");
+
+	     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+}
---
+++ JAVA _PROGRAMMING_ASSESMENT_1/Add.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Add{
+	public static void main(String[]args){
+           	Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a 2 numbers :");
+		int a=sc.nextInt();
+ 		int b = sc.nextInt();
+		
+		System.out.println("Addition of 2 numbers is :"+(a+b));
+
+
+
+  }
+}
